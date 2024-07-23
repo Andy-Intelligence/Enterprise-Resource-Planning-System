09a13803bd4bb5eff21306bb25807d7d36ea92bb
Andidiong <usoroandidiong@gmail.com>
2024-07-23 15:53:40 +0100

    sidebar configure

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { sidebarLinks } from "@/constants";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
